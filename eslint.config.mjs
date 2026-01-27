cb94cb93735879ce02fb017094e0767fdbbfcb79
aisl1n <aislanarchy@gmail.com>
2026-01-26 23:07:35 -0300

    chore: update ESLint configuration and add unused-imports plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config"
+import nextVitals from "eslint-config-next/core-web-vitals"
+import nextTs from "eslint-config-next/typescript"
+import unusedImports from "eslint-plugin-unused-imports"
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  {
+    plugins: { "unused-imports": unusedImports },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+])
 
-export default eslintConfig;
+export default eslintConfig
